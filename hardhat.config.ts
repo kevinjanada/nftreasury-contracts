faf24b1d78e48197fbb577b0d2b8555bf79d52cd
Kevin Janada <kevin@Kevins-MacBook-Air.local>
2023-01-04 15:10:39 +0700

    Add gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,13 @@
+require("dotenv").config();
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-dependency-compiler";
 import "@openzeppelin/hardhat-upgrades";
+import "hardhat-gas-reporter"
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
   solidity: {
     version: '0.8.11',
     settings: {
@@ -17,6 +21,26 @@ const config: HardhatUserConfig = {
     paths: [
       '@thirdweb-dev/contracts/marketplace/Marketplace.sol',
     ],
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    gasPriceApi: "https://api.etherscan.io/api?module=proxy&action=eth_gasPrice",
+  },
+  etherscan: {
+    apiKey: {
+      // ethereum
+      mainnet: process.env.ETHERSCAN_API_KEY as string,
+      ropsten: process.env.ETHERSCAN_API_KEY as string,
+      rinkeby: process.env.ETHERSCAN_API_KEY as string,
+      goerli: process.env.ETHERSCAN_API_KEY as string,
+      kovan: process.env.ETHERSCAN_API_KEY as string,
+      sepolia: process.env.ETHERSCAN_API_KEY as string,
+      // polygon
+      polygon: process.env.POLYGONSCAN_API_KEY as string,
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY as string,
+    },
   }
 };
 
